705534763b2df1e57cb1ef51093dfede17e78e99
Josh Humphries <j.humphries@nhm.ac.uk>
2019-09-18 12:16:46 +0100

    Add destination input

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const github = require('@actions/github');
 try {
   // `target` input defined in action metadata file
   const target = core.getInput('target');
+  const destination = core.getInput('destination');
   console.log(github.context);
   console.log(`Target: ${target}!`);
+  console.log(`Destination: ${destination}!`);
   core.setOutput("modified_files", 23);
 } catch (error) {
   core.setFailed(error.message);
