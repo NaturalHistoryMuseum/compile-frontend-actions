da371e03d6de478045ce1362e82e12368ac1e971
Josh Humphries <j.humphries@nhm.ac.uk>
2019-09-18 17:04:27 +0100

    Implement separate js and less actions

---
+++ js/index.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+const terser = require('terser');
+const glob = require('glob');
+
+try {
+  // pull out the expected action parameters
+  const target = core.getInput('target');
+  const destination = core.getInput('destination');
+
+  // setup the terser minification options
+  const terserOptions = {
+    compress: true,
+    mangle: true
+  };
+
+  for (let filename of glob.sync(target)) {
+    // skip already minified js files
+    if (filename.endsWith('.min.js')) {
+      continue;
+    }
+
+    // read and then render the javascript source
+    let result = terser.minify(fs.readFileSync(filename, 'utf8'), terserOptions);
+    if (result.error) {
+      throw result.error;
+    }
+    // figure out the name of the output file simply by replacing the .js with .min.js in the file
+    // name
+    let destinationFilename = path.basename(filename).replace('.js', '.min.js');
+    let outputFile = '';
+      if (!destination) {
+        // no destination specified by the user so we default to the same directory as the
+        // javascript source was found in
+        outputFile = path.join(path.dirname(filename), destinationFilename);
+      } else {
+        // if the destination provided by the user ends in a slash, assume they want this path
+        // treated as a directory and add the destination .min.js file name we generated above,
+        // otherwise just use the provided destination as if it's a full file path
+        if (destination.endsWith(path.sep)) {
+          outputFile = path.join(destination, destinationFilename);
+        }
+      }
+      // write the minified js out to the output file location
+      fs.writeFileSync(outputFile, result.code, {'encoding': 'utf8'});
+      console.log(`Minified ${destinationFilename} -> ${outputFile}`);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
---
+++ less/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+const less = require('less');
+const glob = require('glob');
+
+try {
+  // pull out the expected action parameters
+  const target = core.getInput('target');
+  const destination = core.getInput('destination');
+
+  for (let filename of glob.sync(target)) {
+    const lessOptions = {
+      // this sets the context for less so it knows where to look for relative imports in the less
+      'paths': [path.dirname(filename)]
+    };
+    // read and then render the less source
+    less.render(fs.readFileSync(filename, 'utf8'), lessOptions).then(output => {
+        // figure out the name of the output file simply by replacing the .less with .css in the
+        // file name
+        let destinationFilename = path.basename(filename).replace('.less', '.css');
+        let outputFile = '';
+        if (!destination) {
+          // no destination specified by the user so we default to the same directory as the less
+          // source was found in
+          outputFile = path.join(path.dirname(filename), destinationFilename);
+        } else {
+          // if the destination provided by the user ends in a slash, assume they want this path
+          // treated as a directory and add the destination css file name we generated above,
+          // otherwise just use the provided destination as if it's a full file path
+          if (destination.endsWith(path.sep)) {
+            outputFile = path.join(destination, destinationFilename);
+          }
+        }
+        // write the css out to the output file location
+        fs.writeFileSync(outputFile, output.css, {'encoding': 'utf8'});
+        console.log(`Compiled ${destinationFilename} -> ${outputFile}`);
+      }).catch(err => {
+        core.setFailed(err.message);
+      });
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
