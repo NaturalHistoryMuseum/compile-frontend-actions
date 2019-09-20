8d2addab7fccf8d176abd0c0e3c8635726afe35a
Josh Humphries <j.humphries@nhm.ac.uk>
2019-09-20 09:34:00 +0100

    Tidy up some of the comments and logic in js processing code

--- js/index.js
+++ js/index.js
@@ -11,13 +11,16 @@ try {
   const destination = core.getInput('destination');
   let modified = core.getInput('modified');
 
+  // modified is expected to be a json serialised list of files that have been modified in the step
+  // chain so far
   try {
     modified = JSON.parse(modified);
   } catch (err) {
+    // if it was not provided or was crap, default it to []
     modified = [];
   }
 
-  // setup the terser minification options
+  // setup the terser minification options (these are the defaults but might as well be explicit)
   const terserOptions = {
     compress: true,
     mangle: true
@@ -34,28 +37,31 @@ try {
     if (result.error) {
       throw result.error;
     }
-    // figure out the name of the output file simply by replacing the .js with .min.js in the file
-    // name
-    let destinationFilename = path.basename(filename).replace('.js', '.min.js');
-    let outputFile = '';
-    if (!destination) {
+
+    // figure out the default name of the output file simply by replacing the .js with .min.js in
+    // the file name
+    const defaultFilename = path.basename(filename).replace('.js', '.min.js');
+    // default to write location of the minified js to the destination provided by the caller
+    let outputFile = destination;
+    if (!outputFile) {
       // no destination specified by the user so we default to the same directory as the
       // javascript source was found in
-      outputFile = path.join(path.dirname(filename), destinationFilename);
+      outputFile = path.join(path.dirname(filename), defaultFilename);
     } else {
       // if the destination provided by the user ends in a slash, assume they want this path
-      // treated as a directory and add the destination .min.js file name we generated above,
-      // otherwise just use the provided destination as if it's a full file path
-      if (destination.endsWith(path.sep)) {
-        outputFile = path.join(destination, destinationFilename);
+      // treated as a directory and add the destination .min.js file name we generated above
+      if (outputFile.endsWith(path.sep)) {
+        outputFile = path.join(destination, defaultFilename);
       }
     }
+
     // write the minified js out to the output file location
     fs.writeFileSync(outputFile, result.code, { 'encoding': 'utf8' });
-    console.log(`Minified ${destinationFilename} -> ${outputFile}`);
+    console.log(`Minified ${path.basename(filename)} -> ${outputFile}`);
     modified.push(outputFile);
   }
 
+  // pass on the modified output
   core.setOutput('modified', JSON.stringify(modified));
 } catch (error) {
   core.setFailed(error.message);
